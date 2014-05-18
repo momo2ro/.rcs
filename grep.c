bc1c6bd766aacc156accfd564c653fd809c0dc68
桃次郎 <mpwf.fwbohfmjpo.1904@gmail.com>
2014-05-18 12:18:02 +0900

    modified:   grep.c 	modified:   grep.o

--- grep.c
+++ grep.c
@@ -3,14 +3,37 @@
 #include <sys/types.h>
 #include <regex.h>
 
-static void do_grep(regex_t *pat, FILE *f);
+#define _GNU_SOURCE
+#include <getopt.h>
+
+static void do_grep(regex_t *pat, FILE *f, int g);
 
 int main(int argc, char *argv[]) {
     regex_t pat;
     int err;
     int i;
+    int opt;
+    int g = 0;
 
-    err = regcomp(&pat,argv[1], REG_EXTENDED | REG_NOSUB | REG_NEWLINE);
+    while ((opt = getopt(argc, argv, "iv")) != -1) {
+        i = optind;
+        switch (opt) {
+            case 'i':
+                err = regcomp(&pat,argv[i], REG_EXTENDED | REG_NOSUB | REG_NEWLINE | REG_ICASE);
+                break;
+            case 'v':
+                err = regcomp(&pat,argv[i], REG_EXTENDED | REG_NOSUB | REG_NEWLINE);
+                g = 1;
+                break;
+            case '?':
+                fprintf(stderr, "Usage: %s [-i] [-v] [patten] [file ...]\n", argv[0]);
+                exit(1);
+            default:
+                err = regcomp(&pat,argv[i], REG_EXTENDED | REG_NOSUB | REG_NEWLINE);
+                break;
+        }
+    }
+
     if (err != 0) {
         char buf[1024];
 
@@ -18,10 +41,11 @@ int main(int argc, char *argv[]) {
         puts(buf);
         exit(1);
     }
-    if (argc == 2) {
-        do_grep(&pat, stdin);
+    i = i + 1;
+    if (argc == i) {
+        do_grep(&pat, stdin, g);
     } else {
-        for (i = 2; i < argc; i++) {
+        for (i = i; i < argc; i++) {
             FILE *f;
 
             f = fopen(argv[i], "r");
@@ -29,7 +53,7 @@ int main(int argc, char *argv[]) {
                 perror(argv[i]);
                 exit(1);
             }
-            do_grep(&pat, f);
+            do_grep(&pat, f, g);
             fclose(f);
         }
     }
@@ -38,13 +62,20 @@ int main(int argc, char *argv[]) {
 }
 
 
-static void do_grep(regex_t *pat, FILE *src) {
+static void do_grep(regex_t *pat, FILE *src, int g) {
     char buf[4096];
 
     while (fgets(buf, sizeof buf, src)) {
-        if (regexec(pat, buf, 0, NULL, 0) == 0) {
-            fputs(buf, stdout);
+        if (g == 0) {
+            if (regexec(pat, buf, 0, NULL, 0) == 0) {
+                fputs(buf, stdout);
+            }
+        } else {
+            if (regexec(pat, buf, 0, NULL, 0) == REG_NOMATCH) {
+                fputs(buf, stdout);
+            }
         }
+
     }
 }
 
